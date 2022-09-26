3d4932b86eecca67b9b186791d4d05830abd4e73
Shreyas Sambasivam <shreyas@Shreyass-MacBook-Air.local>
2022-09-25 20:34:50 -0400

    remove controller test

--- src/test/java/com/sjs7477/shopeazy/HelloWorldControllerTest.java
+++ src/test/java/com/sjs7477/shopeazy/HelloWorldControllerTest.java
@@ -1,23 +1,18 @@
 package com.sjs7477.shopeazy;
 
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloWorldControllerTest {
 @LocalServerPort
     private int port;
-@Autowired
-    private TestRestTemplate testRestTemplate;
-@Test
-    public void HelloWorldCheck(){
-    assertEquals("Hello World from Spring Boot!!!",testRestTemplate.getForObject("http://localhost:"+port+"/",
-            String.class));
-}
+//@Autowired
+//    private TestRestTemplate testRestTemplate;
+//@Test
+//    public void HelloWorldCheck(){
+//    assertEquals("Hello World from Spring Boot!!!",testRestTemplate.getForObject("http://localhost:"+port+"/",
+//            String.class));
+//}
 }
